bcfdb5805938f67dffe68488bfb36c8fb4012794
double <sjliu0920@163.com>
2016-08-24 21:17:17 +0800

    add utf8

--- initbd.sh
+++ initbd.sh
@@ -2,18 +2,19 @@
 HOSTNAME="127.0.0.1"  #数据库信息
 PORT="3306"
 USERNAME="root"
-PASSWORD=""
+PASSWORD="lsj"
 
 DBNAME="costco"  #数据库名称
 TYPE_TABLE_NAME="type_table" #数据库中表的名称
 ITEM_TABLE_NAME="item_table"
+USER_TABLE_NAME="user_table"
 
 #drop costco db
 drop_costco_sql="drop database IF EXISTS ${DBNAME}"
 mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} -e "${drop_costco_sql}"
 
 # create costco db
-create_costco_sql="create database IF NOT EXISTS ${DBNAME}"
+create_costco_sql="create database ${DBNAME} CHARACTER SET utf8"
 mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} -e "${create_costco_sql}"
 
 # create type table
@@ -23,7 +24,7 @@ create table IF NOT EXISTS ${TYPE_TABLE_NAME}
 	type_id		int NOT NULL AUTO_INCREMENT,
 	type_name	varchar(20), 
 	PRIMARY KEY (type_id)
-) ENGINE=InnoDB;"
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
 
 create_item_table_sql="
 create table IF NOT EXISTS ${ITEM_TABLE_NAME} 
@@ -34,7 +35,16 @@ create table IF NOT EXISTS ${ITEM_TABLE_NAME}
 	price		double,
 	star		int,
 	PRIMARY KEY (item_id)
-) ENGINE=InnoDB;"
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
+
+create_user_table_sql="
+create table IF NOT EXISTS ${USER_TABLE_NAME} 
+(
+	user_id		int NOT NULL AUTO_INCREMENT,
+	account		varchar(20), 
+	passwd		varchar(20), 
+	PRIMARY KEY (user_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
 
 
 insert_type_sql="insert into ${TYPE_TABLE_NAME} values 
@@ -47,8 +57,13 @@ insert_item_sql="insert into ${ITEM_TABLE_NAME} values
 ( NULL, 0, '金鸳鸯', 13.99, 4),
 ( NULL, 0, '阿迪达斯', 14.99, 5);"
 
+insert_user_sql="insert into ${USER_TABLE_NAME} values 
+( NULL, 'double', 'lsj');"
+
 mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${create_type_table_sql}"
 mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${create_item_table_sql}"
+mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${create_user_table_sql}"
 mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_type_sql}"
 mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_item_sql}"
+mysql -h${HOSTNAME} -P${PORT} -u${USERNAME} -p${PASSWORD} ${DBNAME} -e "${insert_user_sql}"
 
